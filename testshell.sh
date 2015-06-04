97c7fbac8e55f3074fb91d150b4f3369d58499dd
EC2 Default User <ec2-user@ip-172-31-0-10.ap-northeast-1.compute.internal>
2015-06-04 17:55:20 +0000

    add 変数表示

--- testshell.sh
+++ testshell.sh
@@ -4,7 +4,11 @@
 ## WORDPRESS設定ファイルのパス
 WP_CONF_PATH='/var/www/html/wp-config.php'
 
-## 自分に割り当てられたPublicIPを取得
+## LOGファイルのパス
+LOG_NAME='wp-ip-reset.log'
+LOG_PATH="$(pwd)/${LOG_NAME}"
+
+# 自分に割り当てられたPublicIPを取得
 PUBLIC_IP="'`curl -s http://169.254.169.254/latest/meta-data/public-ipv4/`'"
 
 ## DB情報取得
@@ -13,6 +17,7 @@ DB_USER=`grep "^define('DB_USER" ${WP_CONF_PATH} | sed -e "s/define('DB_USER', '
 DB_PASSWORD=`grep "^define('DB_PASSWORD" ${WP_CONF_PATH} | sed -e "s/define('DB_PASSWORD', '\(.*\)');/\1/" | tr -d "\r"`
 DB_HOST=`grep "^define('DB_HOST" ${WP_CONF_PATH} | sed -e "s/define('DB_HOST', '\(.*\)');/\1/" | tr -d "\r"`
 
+echo ${LOG_PATH} ${PUBLIC_IP} ${DB_NAME} ${DB_USER} ${DB_PASSWORD} ${DB_HOST}
 
 ## DB接続
 mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'siteurl';"
