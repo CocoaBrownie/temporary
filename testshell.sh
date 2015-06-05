8376bd3171320667b2232cde4f8963f2675369ba
CocoaBrownie <cocoabrownie@gmail.com>
2015-06-05 23:28:27 +0900

    trivial
    ロギングの冒頭に改行を追加
    パスワードをロギングしない

--- testshell.sh
+++ testshell.sh
@@ -9,7 +9,7 @@ LOG_NAME='wp-ip-reset.log'
 LOG_PATH="$(pwd)/${LOG_NAME}"
 
 # 自分に割り当てられたPublicIPを取得
-PUBLIC_IP="'`curl -s http://169.254.169.254/latest/meta-data/public-ipv4/`'"
+PUBLIC_IP="'http://`curl -s http://169.254.169.254/latest/meta-data/public-ipv4/`'"
 
 ## DB情報取得
 DB_NAME=`grep "^define('DB_NAME" ${WP_CONF_PATH} | sed -e "s/define('DB_NAME', '\(.*\)');/\1/" | tr -d "\r"`
@@ -18,21 +18,21 @@ DB_PASSWORD=`grep "^define('DB_PASSWORD" ${WP_CONF_PATH} | sed -e "s/define('DB_
 DB_HOST=`grep "^define('DB_HOST" ${WP_CONF_PATH} | sed -e "s/define('DB_HOST', '\(.*\)');/\1/" | tr -d "\r"`
 
 ## LOGING
-echo "\n" >> ${LOG_PATH}
+echo -e "\n" >> ${LOG_PATH}
 echo '####################' >> ${LOG_PATH}
 echo 実行日時：`date` >> ${LOG_PATH}
 echo LOG_PATH      ${LOG_PATH} >> ${LOG_PATH}
 echo PUBLIC_IP     ${PUBLIC_IP} >> ${LOG_PATH}
 echo DB_NAME       ${DB_NAME} >> ${LOG_PATH}
 echo DB_USER       ${DB_USER} >> ${LOG_PATH}
-echo DB_PASSWORD   ${DB_PASSWORD} >> ${LOG_PATH}
+#echo DB_PASSWORD   ${DB_PASSWORD} >> ${LOG_PATH}
 echo DB_HOST       ${DB_HOST} >> ${LOG_PATH}
 
 ## DB接続
 mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"select * from wp_options where option_name = 'siteurl';" >> ${LOG_PATH}
 mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"select * from wp_options where option_name = 'home';" >> ${LOG_PATH}
 
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'siteurl';" >> ${LOG_PATH}
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"update wp_options set option_value = http://${PUBLIC_IP} where option_name = 'siteurl';" >> ${LOG_PATH}
 mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'home';" >> ${LOG_PATH}
 
 mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} --connect_timeout=5 -e"select * from wp_options where option_name = 'siteurl';" >> ${LOG_PATH}
