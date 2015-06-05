f9e6a3283f90cbf352002fc27ac1f265ce827cc4
CocoaBrownie <cocoabrownie@gmail.com>
2015-06-05 19:49:42 +0900

    add loging

--- testshell.sh
+++ testshell.sh
@@ -17,17 +17,25 @@ DB_USER=`grep "^define('DB_USER" ${WP_CONF_PATH} | sed -e "s/define('DB_USER', '
 DB_PASSWORD=`grep "^define('DB_PASSWORD" ${WP_CONF_PATH} | sed -e "s/define('DB_PASSWORD', '\(.*\)');/\1/" | tr -d "\r"`
 DB_HOST=`grep "^define('DB_HOST" ${WP_CONF_PATH} | sed -e "s/define('DB_HOST', '\(.*\)');/\1/" | tr -d "\r"`
 
-echo ${LOG_PATH} ${PUBLIC_IP} ${DB_NAME} ${DB_USER} ${DB_PASSWORD} ${DB_HOST}
+## LOGING
+echo '####################' >> ${LOG_PATH}
+echo 実行日時：`date` >> ${LOG_PATH}
+echo LOG_PATH      ${LOG_PATH} >> ${LOG_PATH}
+echo PUBLIC_IP     ${PUBLIC_IP} >> ${LOG_PATH}
+echo DB_NAME       ${DB_NAME} >> ${LOG_PATH}
+echo DB_USER       ${DB_USER} >> ${LOG_PATH}
+echo DB_PASSWORD   ${DB_PASSWORD} >> ${LOG_PATH}
+echo DB_HOST       ${DB_HOST} >> ${LOG_PATH}
 
 ## DB接続
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'siteurl';"
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'home';"
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'siteurl';" >> ${LOG_PATH}
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'home';" >> ${LOG_PATH}
 
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'siteurl';"
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'home';"
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'siteurl';" >> ${LOG_PATH}
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"update wp_options set option_value = ${PUBLIC_IP} where option_name = 'home';" >> ${LOG_PATH}
 
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'siteurl';"
-mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'home';"
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'siteurl';" >> ${LOG_PATH}
+mysql -h ${DB_HOST} -D ${DB_NAME} -u ${DB_USER} -p${DB_PASSWORD} -e"select * from wp_options where option_name = 'home';" >> ${LOG_PATH}
 
 
 ## 終了
